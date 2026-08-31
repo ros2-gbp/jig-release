4e38e6042e9224bd02f507ccfd002ec984d63535
Alistair English <hello@alistairenglish.com>
2026-08-31 22:58:30 +0000

    Trimmed the branch to only the jig sub directory

--- jig_cli/tests/test_ws/src/pkg_gamma/nodes/cpp_node/cpp_node.cpp
+++
@@ -1,7 +0,0 @@
-#include "cpp_node.hpp"
-
-namespace pkg_gamma::cpp_node {
-
-CallbackReturn on_configure(std::shared_ptr<Session> /*sn*/) { return CallbackReturn::SUCCESS; }
-
-} // namespace pkg_gamma::cpp_node
