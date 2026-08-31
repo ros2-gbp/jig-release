d633cfcc522540525f1a34bed6176a1e5289c83e
Alistair English <hello@alistairenglish.com>
2026-08-31 22:58:29 +0000

    Trimmed the branch to only the jig_cli sub directory

--- jig/tests/fixtures/subscribers_only/expected_python/parameters.py
+++
@@ -1,9 +0,0 @@
-# auto-generated DO NOT EDIT
-
-from ._parameters import parameters
-
-# Flatten the nested structure for cleaner API
-Params = parameters.Params
-ParamListener = parameters.ParamListener
-
-__all__ = ["Params", "ParamListener"]
